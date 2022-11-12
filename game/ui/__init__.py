d33f5df7a6e632b9777b065e67bc829c379c9c64
Jeffrey Yurkiw <jyurkiw@yahoo.com>
2022-11-11 21:34:42 -0800

    Added a main menu handler

--- game/ui/__init__.py
+++ game/ui/__init__.py
@@ -17,8 +17,34 @@ class TitleUI(UI):
 
     def __init__(self, parent: py_cui.PyCUI):
         self.parent = parent
+
         self.ui = self.parent.create_new_widget_set(8, 5)
-        self.title = self.ui.add_text_block("Arandmoor's", 0, 1, column_span=3, initial_text=TitleUI.title)
+
+        self.title = self.ui.add_text_block("Arandmoor's", 0, 0, column_span=5, initial_text=TitleUI.title)
+
+        self.menu_options = ['New Game', 'Load Game', 'Help', 'Settings', 'Quit']
+
+        self.menu = self.ui.add_scroll_menu('Main Menu', 1, 2, row_span=2)
+        self.menu.add_item_list(self.menu_options)
+        self.menu.add_key_command(py_cui.keys.KEY_ENTER, self.main_menu_handler)
+        self.menu.set_selected(True)
+
+    def main_menu_handler(self):
+        option = self.menu.get()
+
+        if option == 'New Game':
+            pass
+        elif option == 'Load Game':
+            pass
+        elif option == 'Help':
+            self.parent.show_message_popup(
+                title='Help',
+                text='github.com/jyurkiw/Guildmaster/docs/toc.md',
+                color=py_cui.WHITE_ON_BLACK)
+        elif option == 'Settings':
+            pass
+        elif option == 'Quit':
+            exit()
 
     def enable(self):
         self.parent.apply_widget_set(self.ui)
